3e6f7165ebb852211322e682d2a7bb121ac8a123
ripoll97 <miquel97ripoll@gmail.com>
2017-05-27 16:36:13 +0200

    Simulation done

--- src/physics.cpp
+++ src/physics.cpp
@@ -19,7 +19,7 @@ vec3 *waveInitialVertexPosition;
 #pragma region Variables
 
 float separationX = 1, separationY = 1;
-float gravity = -9.8;
+float gravity = -4.9;
 
 float stretchD = 50, shearD = 50, bendD = 50, stretchK = 1000, shearK, bendK;
 float elasticity = 0.5;
@@ -37,11 +37,15 @@ vec3 sphereOnWavePosition = vec3(0.0f);
 vec3 initialSpherePosition;
 float sphereMass = 1;
 float sphereArea = 3.14 * (sphereRadius * sphereRadius);
+vec3 sphereVelocity;
 
-//Buoyancy parameters
-float p = 1; //Density of the fluid
+//Forces parameters
+float p = 2.5; //Density of the fluid
 float vSub; //Volume "submergit" of the sphere
+float dragCoeff = 0.75; //Give a value between 0.1 and 1.1
 
+void Restart();
+float timeCounter;
 #pragma endregion
 
 bool show_test_window = false;
@@ -49,8 +53,9 @@ bool show_test_window = false;
 void GUI() {
 	{	//FrameRate
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-
-		//TODO
+		ImGui::SliderFloat("Sphere Mass", &sphereMass, 1, 10);
+		ImGui::SliderFloat("Drag coefficient", &dragCoeff, 0.2, 1.1);
+		ImGui::InputFloat("Fluid density", &p);
 	}
 
 	// ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
@@ -81,37 +86,37 @@ vec3 * CreateClothMeshArray(int rowVerts, int columnVerts, float vertexSeparatio
 }
 
 float BuoyancyForce() {
-
-	float subHeight = sphereOnWavePosition.y - (spherePosition.y - sphereRadius);
-
 	float diameter = sphereRadius * 2;
+
+	float subHeight = clamp(sphereOnWavePosition.y - (spherePosition.y - sphereRadius), 0.f, diameter);
+
 	//vSub = (3.14 / 3) * (subHeight * subHeight) * (1.5 * (sphereRadius * 2) - subHeight);
 	vSub = diameter * diameter * subHeight;
-
+	
 	return (p * (4.9) * vSub);
 }
 
-float DragForce() {
-
-
-	return (-1 / 2) * 1;
+vec3 DragForce() {
+	vec3 velocityDiff = sphereVelocity;
+	return (-0.5f * p * (length(velocityDiff)) * sphereArea * dragCoeff) * sphereVelocity;
 }
 
 void UpdateSphere(float dt) {
-	float totalForce;
-	float gravity = sphereMass * -9.8;
-
+	vec3 totalForce;
 	sphereOnWavePosition = spherePosition;
-	sphereOnWavePosition.y = A * cos(dot(waveVector, initialSpherePosition) - omega * time) + 5;
-	
+	sphereOnWavePosition.y = A * cos(dot(waveVector, initialSpherePosition) - omega * time) + 5;	
+
 	if ((spherePosition.y - sphereRadius) <= sphereOnWavePosition.y) {
-		totalForce = gravity + BuoyancyForce();
+		totalForce.y = (gravity * sphereMass) + BuoyancyForce();
+		totalForce += DragForce();
 	}
 	else
-		totalForce = gravity;
+		totalForce.y = gravity;
+	
+	sphereVelocity += totalForce * dt;
 
 	// Euler movement
-	spherePosition.y = spherePosition.y + dt * totalForce;
+	spherePosition.y += sphereVelocity.y * dt;
 }
 
 void PhysicsInit() {
@@ -123,6 +128,7 @@ void PhysicsInit() {
 	initialSpherePosition = vec3(spherePosition.x, 5, spherePosition.z);
 
 }
+
 void PhysicsUpdate(float dt) {
 	//TODO
 	time += dt;
@@ -134,7 +140,26 @@ void PhysicsUpdate(float dt) {
 
 	UpdateSphere(dt);
 	ClothMesh::updateClothMesh(&waveVertexPosition[0].x);
+
+	if (ImGui::Button("Restart") || timeCounter >= 20) {
+		Restart();
+	}
+	timeCounter += dt;
 }
 void PhysicsCleanup() {
 	//TODO
+}
+
+void Restart() {
+	sphereVelocity = vec3(0.0);
+	spherePosition.x = -5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (5 - -5)));
+	spherePosition.y = 6 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (10 - 6)));
+	spherePosition.z = -5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (5 - -5)));
+	
+	vec3 meshPosition(-8.5, 5, -6.5);
+	waveInitialVertexPosition = CreateClothMeshArray(ClothMesh::numRows, ClothMesh::numCols, separationX, separationY, meshPosition);
+	waveVertexPosition = CreateClothMeshArray(ClothMesh::numRows, ClothMesh::numCols, separationX, separationY, meshPosition);
+
+	initialSpherePosition = vec3(spherePosition.x, 5, spherePosition.z);
+	timeCounter = 0;
 }
